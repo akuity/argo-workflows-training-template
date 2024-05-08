d63f6b969b6fd2ccef518d167bf9807c2a454b91
Jesse Suen <jesse@akuity.io>
2024-05-08 15:56:13 -0700

    fix: use node ports
    Signed-off-by: Jesse Suen <jesse@akuity.io>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,10 +4,8 @@
 echo "post-create start" >> ~/.status.log
 
 ## Create k3s cluster
-export K3S_VERSION=v1.27.13-k3s1
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-k3d cluster get k3s-default || k3d cluster create --image rancher/k3s:${K3S_VERSION} --wait | tee -a ~/.status.log
-k3d kubeconfig merge --kubeconfig-merge-default | tee -a ~/.status.log
+k3d cluster create --config .devcontainer/k3d.yaml --wait | tee -a ~/.status.log
 
 ## Install
 kustomize build ./install/argo-workflows | kubectl apply -f - | tee -a ~/.status.log
